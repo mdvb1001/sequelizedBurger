0f1e0627efe258f976e1b8f089c1fb3a72cbc530
maxvanbel <maxvanbel@gmail.com>
2017-01-29 22:48:28 -0600

    controller work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,22 +5,26 @@ module.exports = function (app) {
     // Get the root route
     app.get("/", function (req, res) {
         if (req.params.devoured === false) {
-        Burger.getAll({
-            where: {
-                devoured: false
-            }
-        }).then(function (result){
-            return res.render("index", {burgers: result});
-        });
-    } else {
-        Burger.getAll({
-            where: {
-                devoured: true
-            }
-        }).then(function (result){
-            return res.render("index", {devoured_burgers: result});
-        });
-    }
+            Burger.getAll({
+                where: {
+                    devoured: false
+                }
+            }).then(function (result) {
+                return res.render("index", {
+                    burgers: result
+                });
+            });
+        } else {
+            Burger.getAll({
+                where: {
+                    devoured: true
+                }
+            }).then(function (result) {
+                return res.render("index", {
+                    devoured_burgers: result
+                });
+            });
+        }
     });
     // Get the API route, which displays all burgers
     app.get("/api/burgers/:id?", function (req, res) {
@@ -41,24 +45,37 @@ module.exports = function (app) {
             });
         }
     });
+    // Post for creating burger
+    app.post('/', function (req, res) {
+        var newBurg = req.body.burger;
+        // Makes sure something is inputed
+        if (newBurg !== "") {
+            Burger.create({
+                where: {
+                    burger_name: newBurg.burger_name
+                }
+            }).then(function (result) {
+                return res.json(result);
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+    // Defines the updates for when burgers are "devoured"
+    app.put('/:id', function (req, res) {
+        var selectBurg = req.params.id;
+        Book.findOne({
+            where: {
+                title: selectBurg
+            }
+        }).then(function (result) {
+            Burger.update({
+                where: {
+                    devoured: true
+                }
+            }).then(function (result){
+                return res.json(result);
+            });
+        });
+    });
 };
-// Post for creating burger
-// app.post('/', function (req, res) {
-//     var newBurg = req.body.burger;
-//     // Makes sure something is inputed
-//     if (newBurg !== "") {
-//         burgerFunctions.addBurger(newBurg, function () {
-//             res.redirect('/');
-//         });
-//     } else {
-//         res.redirect('/');
-//     }
-// });
-// // Defines the updates for when burgers are "devoured"
-// app.put('/:id', function (req, res) {
-//     var selectBurg = req.params.id;
-//     burgerFunctions.devourBurger(selectBurg, function (data) {
-//         res.redirect('/');
-//     });
-// });
-// };
